0491a666299e183a8247850e526427aa61fb9c95
Natalie Till <natalieannatill@gmail.com>
2023-07-07 14:51:48 +0100

    Inital Very Basic Frontend created

---
+++ components/Navbar.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import Logo from '../components/logo.png'
+
+export default function Navbar() {
+    return (
+        <nav className="bg-slate-600 p-3 flex justify-center flex-row md:flex-row sticky top-0 drop-shadow-xl">
+            <h1 className="text-3xl font-bold text-white grid place-content-center pl-3 mb-2 md:mb-0">
+                <Link href="/">Vision</Link>
+            </h1>
+            <Image 
+                className="flex-image p-2"
+                src={Logo}
+                alt="Vision Logo"
+                width="75"
+            />
+        </nav>
+    )
+}
--- components/guide.md
+++
@@ -1,3 +0,0 @@
-## Components directory:
-
-Use this directory for adding your components.
