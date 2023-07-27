15fa552d441d81ebecdee02181f3fc3e0e8dd0a8
gsherrin <jennasherrington1@gmail.com>
2023-07-27 12:38:15 +0200

    feat/layout

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,11 +1,11 @@
 import Header from "@/components/Header";
-import Landing from "@/components/Landing";
+import Projects from "@/components/Projects";
 
 export default function Home() {
   return (
     <>
     < Header></Header>
-    < Landing></Landing>
+    < Projects> </Projects>
     </>
   )
 }
